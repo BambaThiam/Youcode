163d1db3d4cc57d7aed56932e4cbeefeb7d3771d
DayOneAI <bambathiam88@gmail.com>
2024-04-03 23:45:14 +0200

    feat: ✨Setup React Query #1

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,20 +1,26 @@
 // app/Providers.
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { PropsWithChildren } from 'react';
 import { Toaster } from 'sonner';
 
 
+const queryClient = new QueryClient();
+
+
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Toaster />
-        <SessionProvider>{children}</SessionProvider>
-      </ThemeProvider>
-    </>
+    
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Toaster />
+          <SessionProvider>{children}</SessionProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    
   );
 };
 
